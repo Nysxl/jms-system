458b021e16557f35a01b44ac81e0d2a3cdedc62c
Nysxl <109190345+Nysxl@users.noreply.github.com>
2026-06-12 06:33:35 +1000

    Update index.tsx

--- jms-project/pages/index.tsx
+++ jms-project/pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { supabase } from '@/lib/supabase';
 
 export default function Home() {
   const router = useRouter();
