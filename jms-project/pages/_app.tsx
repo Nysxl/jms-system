9c96f04a5b5df067a0560a8e8c479cea5e135cbc
Nysxl <109190345+Nysxl@users.noreply.github.com>
2026-06-12 05:53:06 +1000

    Update _app.tsx

--- jms-project/pages/_app.tsx
+++ jms-project/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import '../styles/stylesglobals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
