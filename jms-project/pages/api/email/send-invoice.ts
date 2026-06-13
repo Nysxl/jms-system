41f66097032d2c565b4e0d150bca424ede82697a
Nysxl <109190345+Nysxl@users.noreply.github.com>
2026-06-13 15:51:27 +1000

    2

--- jms-project/pages/api/email/send-invoice.ts
+++ jms-project/pages/api/email/send-invoice.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
   return res.status(501).json({ error: 'Email functionality requires resend package. Install with: bun add resend' });
 }
