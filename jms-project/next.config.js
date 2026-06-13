468e6a6c0a60728f5f796ee064f3cfddade6f810
Nysxl <109190345+Nysxl@users.noreply.github.com>
2026-06-13 18:56:03 +1000

    compressed

--- jms-project/next.config.js
+++ jms-project/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  productionBrowserSourceMaps: false,
+  poweredByHeader: false,
+  compress: true,
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 5,
+  },
 };
 
 module.exports = nextConfig;
